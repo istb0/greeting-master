aed0ecca7b4252a546484507066d17e4cfb50e2e
istb0 <yamayosoou@gmail.com>
2022-01-29 15:25:13 +0900

    Add: 録音ページの実装 #7

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -3,4 +3,8 @@ class GreetingsController < ApplicationController
   def index
     @greetings = Greeting.all
   end
+
+  def show
+    @greeting = Greeting.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'top#index'
 
-  resources :greetings, only: %i[index]
+  resources :greetings, only: %i[index show]
 end
