d7b3bae53825a0ec06bb01e0da05a3af80f4e0ff
istb0 <yamayosoou@gmail.com>
2022-02-19 01:12:03 +0900

    Add: Resultsテーブルの作成 #37

--- app/models/greeting.rb
+++ app/models/greeting.rb
@@ -1,4 +1,6 @@
 class Greeting < ApplicationRecord
+  has_many :results, dependent: :destroy
+
   validates :phrase, length: { maximum: 50 }, presence: true
   validates :subtitle, length: { maximum: 255 }
 end
