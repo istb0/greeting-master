d926789de8b746627095726ee1403802fb94a900
istb0 <yamayosoou@gmail.com>
2022-02-19 01:16:39 +0900

    Fix: 各ビューの値をDBから持ってくるように修正 #37

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'top#index'
 
-  resources :greetings, only: %i[index show]
-  post 'analyse', to: 'results#analyse'
-  get 'result', to: 'results#show'
-  post 'result', to: 'results#show'
+  resources :greetings, only: %i[index show] do
+    resources :results, only: %i[show create], shallow: true
+  end
 end
