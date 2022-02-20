d7191c068241600b97bb840dd263d9d7b86ddb49
istb0 <yamayosoou@gmail.com>
2022-02-20 20:07:55 +0900

    Add: font Awesomeの導入 #39

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "bootstrap";
+import '@fortawesome/fontawesome-free/js/all';
 import "../stylesheets/application.scss";
 
 Rails.start()
