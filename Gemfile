0ba8e226c4840bd68d5c59e3dc02c96886ffc0f0
istb0 <yamayosoou@gmail.com>
2022-02-27 23:24:32 +0900

    Add: Sorceryインストール、マイグレーションまで #55

--- Gemfile
+++ Gemfile
@@ -32,6 +32,7 @@ gem 'faraday'
 gem 'faraday-multipart'
 gem 'seed-fu'
 gem 'meta-tags'
+gem 'sorcery'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_26_072449) do
+ActiveRecord::Schema.define(version: 2022_02_27_071015) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,6 +69,16 @@ ActiveRecord::Schema.define(version: 2022_02_26_072449) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["greeting_id"], name: "index_results_on_greeting_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "crypted_password"
+    t.string "salt"
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
