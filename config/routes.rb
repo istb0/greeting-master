45c86982d3a1d4f9b42a21f38971196efc84177d
istb0 <yamayosoou@gmail.com>
2022-02-28 00:33:51 +0900

    Add: user_sessionsコントローラの作成、ルーティング設定、ナビゲーション追加 #55

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,10 @@ Rails.application.routes.draw do
   resources :greetings, only: %i[index show] do
     resources :results, only: %i[show create], shallow: true
   end
+
   resources :users
+
+  get 'login' => 'user_sessions#new', :as => :login
+  post 'login' => "user_sessions#create"
+  post 'logout' => 'user_sessions#destroy', :as => :logout
 end
