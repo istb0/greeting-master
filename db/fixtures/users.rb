383ea9a04a69f18466107a48c631398d56f3d373
istb0 <yamayosoou@gmail.com>
2022-03-02 22:02:59 +0900

    Add: UserモデルとResultモデルの関連付け #55

---
+++ db/fixtures/users.rb
@@ -0,0 +1,3 @@
+# 既存の場合→User.update()
+User.create!(id: 1, name: 'guest', email: 'guest@example.com',
+  password: 'guest', password_confirmation: 'guest')
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_27_071015) do
+ActiveRecord::Schema.define(version: 2022_03_02_123047) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,7 +68,9 @@ ActiveRecord::Schema.define(version: 2022_02_27_071015) do
     t.bigint "greeting_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", default: 1, null: false
     t.index ["greeting_id"], name: "index_results_on_greeting_id"
+    t.index ["user_id"], name: "index_results_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -84,4 +86,5 @@ ActiveRecord::Schema.define(version: 2022_02_27_071015) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "results", "greetings"
+  add_foreign_key "results", "users"
 end
