7fbfdf8a2c5ab85817e1e68a5cf69ea3ae56f17b
istb0 <yamayosoou@gmail.com>
2022-03-03 11:12:01 +0900

    Improve: 初期ユーザーの更新処理を追加 #55

--- db/fixtures/users.rb
+++ db/fixtures/users.rb
@@ -1,3 +1,7 @@
-# 既存の場合→User.update()
-User.create!(id: 1, name: 'guest', email: 'guest@example.com',
-  password: 'guest', password_confirmation: 'guest')
+if User.exists?(1)
+  User.update(id: 1, name: 'guest', email: 'guest@example.com',
+    password: 'guest', password_confirmation: 'guest')
+else
+  User.create!(id: 1, name: 'guest', email: 'guest@example.com',
+    password: 'guest', password_confirmation: 'guest')
+end
