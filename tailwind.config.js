a47c883c3391d11f2f598284c1c86d9ddb2f1ae4
istb0 <yamayosoou@gmail.com>
2022-03-05 18:24:45 +0900

    Add: 色、フォントの追加 #69

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        ariel: '#aad6ec',
+        aliceblue: '#f0f8ff',
+        taikoh: '#f69c9f',
+        nibi: '#656765',
+        zincdust: '#5b5b5b',
+        kamenozoki: '#a5dee4',
+        wasurenagusa: '#7db9de',
+        shironeri: '#fcfaf2'
+      },
+    },
   },
   variants: {
     extend: {},
