5412d928a5fae776344cc5a244bb02333fee3a15
istb0 <yamayosoou@gmail.com>
2022-03-08 11:22:53 +0900

    Improve: Bootstrapの削除 #69

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,6 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import "bootstrap";
 import '@fortawesome/fontawesome-free/js/all';
 import "../stylesheets/application.scss";
 import '../css/tailwind.css';
