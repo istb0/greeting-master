451287ccf05dcbaf22a86c3934be68d7bdb9337d
istb0 <yamayosoou@gmail.com>
2022-03-09 23:49:22 +0900

    Add: herokuapp.comからカスタムドメインへリダイレクト、常時SSL化 #76

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,18 @@
 class ApplicationController < ActionController::Base
-  add_flash_types :success, :info, :warning, :danger
-  before_action :require_login
+  before_action :require_login, :ensure_domain
+  FQDN = Rails.application.credentials[:domain]
 
   private
 
   def not_authenticated
-    flash[:warning] = 'ログインしてください'
+    flash[:alert] = 'ログインしてください'
     redirect_to login_path
+  end
+
+  def ensure_domain
+    return unless request.host.match?(/\.herokuapp.com/)
+
+    port = ":#{request.port}" unless [80, 443].include?(request.port)
+    redirect_to "#{request.protocol}#{FQDN}#{port}#{request.path}", status: :moved_permanently
   end
 end
