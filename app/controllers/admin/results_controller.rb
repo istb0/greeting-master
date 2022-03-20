02d30ae482081b08582f9a8be8b8b4c2f4aaf429
istb0 <yamayosoou@gmail.com>
2022-03-21 04:17:02 +0900

    Add: 測定履歴の削除機能を追加 #33

---
+++ app/controllers/admin/results_controller.rb
@@ -0,0 +1,11 @@
+class Admin::ResultsController < Admin::BaseController
+  def index
+    @results = Result.all.includes(:user, :greeting).order(created_at: :desc)
+  end
+  
+  def destroy
+    result = Result.find(params[:id])
+    result.destroy!
+    redirect_to admin_results_path
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -25,5 +25,6 @@ Rails.application.routes.draw do
     post 'login', to: 'user_sessions#create'
     delete 'logout', to: 'user_sessions#destroy'
     resources :users, only: %i[index show edit update destroy]
+    resources :results, only: %i[index destroy]
   end
 end
